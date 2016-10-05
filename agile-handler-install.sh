77bd17fa595fb5d841a0069c0f5c23e107f004f4
david <nopbyte>
2016-10-05 12:11:52 +0200

    update hanlders to check out agile os.js

--- agile-handler-install.sh
+++ agile-handler-install.sh
@@ -1,28 +1,12 @@
-git clone https://github.com/os-js/OS.js
-cd OS.js
-npm install --save https://github.com/Agile-IoT/agile-idm-client
-npm install
-#npm install --production
-grunt create-handler:Agile
-#put handlers!
-cp ../os-js-changes/src/client/javascript/handlers/agile/handler.js  ./src/client/javascript/handlers/agile/handler.js
-cp ../os-js-changes/src/server/node/handlers/agile/handler.js ./src/server/node/handlers/agile/handler.js
-#set agile handler
-grunt config --set=handler --value=agile
+./agile-osjs-install.sh
 
-#Configure AGILE IDM part
-grunt config --set=server.handlers.agile.idm --value=http://localhost:3000/api
-grunt config --set=server.handlers.agile.tokenFile --value=/tmp/token.txt
-grunt config -set=client.Connection.RedirectIDM --value=http://localhost:3000/
-
-grunt dist-dev
 ##################### NODE RED in OS.js #########################33
-cd src/packages/default
-git clone https://github.com/muka/osjs-nodered NodeRed
-cd ../../../
-grunt manifest config packages:default/NodeRed
-grunt dist
-cd ..
+#cd src/packages/default
+#git clone https://github.com/muka/osjs-nodered NodeRed
+#cd ../../../
+#grunt manifest config packages:default/NodeRed
+#grunt dist
+#cd ..
 
 ##################### NODE RED  #########################33
 git clone https://github.com/node-red/node-red
---
+++ agile-osjs-install.sh
@@ -0,0 +1,18 @@
+git clone https://github.com/doitdagi/OS.js
+#git clone https://github.com/os-js/OS.js
+cd OS.js
+npm install --save https://github.com/Agile-IoT/agile-idm-client
+npm install
+#npm install --production
+grunt create-handler:Agile
+#put handlers!
+cp ../os-js-changes/src/client/javascript/handlers/agile/handler.js  ./src/client/javascript/handlers/agile/handler.js
+cp ../os-js-changes/src/server/node/handlers/agile/handler.js ./src/server/node/handlers/agile/handler.js
+#set agile handler
+grunt config --set=handler --value=agile
+#Configure AGILE IDM part
+grunt config --set=server.handlers.agile.idm --value=http://localhost:3000/api
+grunt config --set=server.handlers.agile.tokenFile --value=/tmp/token.txt
+grunt config -set=client.Connection.RedirectIDM --value=http://localhost:3000/
+
+grunt
